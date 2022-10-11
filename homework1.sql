6421631ccd1e115ffeaa0245c43f20e066ed8d8d
Cut666 <110405911+Cut666@users.noreply.github.com>
2022-10-11 22:08:55 +0700

    Add files via upload

---
+++ homework1.sql
@@ -0,0 +1,14 @@
+-- create database quan_ly_kho
+-- create table quan_ly_kho.product(
+-- id int primary key auto_increment,
+-- name varchar(50),
+-- price double,
+-- date datetime
+-- );
+-- insert into quan_ly_kho.product (name,price, date) values ('toyota vios', 520000000, '2022-09-03' )
+-- insert into quan_ly_kho.product (name,price, date) values ('toyota cross', 800000000, '2022-09-03' ),('Huyndai Accent', 510000000, '2022-09-03' ), ('Honda City', 550000000, '2022-09-03' ), ('Vinfast Fadil', 360000000, '2022-09-03' ), ('Mitsubishi Xpander', 620000000, '2022-09-03'), ('Mazda CX-5', 900000000, '2022-09-03' ), ('Kia Seltos', 700000000, '2022-09-03' ), ('kia k3', 650000000, '2022-09-03' ), ('Ford Ranger', 1100000000, '2022-09-03' ), ('Honda CRV', 1100000000, '2022-09-03' ),  ('Hyundai i10', 360000000, '2022-09-03' ), ('Hyundai Santafe', 10000000000, '2022-09-03' ), ('kia carnival', 1800000000, '2022-09-03' ), ('Hyundai Tucson', 800000000, '2022-09-03' )
+-- select * from quan_ly_kho.product where price >=115000000
+-- select * from quan_ly_kho.product where name like '%k%'
+-- select *from quan_ly_kho.product where name like '%kia%' or price <200000000
+-- select * from quan_ly_kho.product where price in (100000000,130000000,140000000,150000000,160000000,170000000,510000000)
+-- select distinct price from quan_ly_kho.product
